73639cb53684152cbd95d6b927474c3b6b7253d9
tanjidor727 <78261510+tanjidor727@users.noreply.github.com>
2021-02-01 12:04:09 +0700

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>191240000910</title>
+
+
+
+</head>
+<body>
+
+
+                       	<table  style="margin-left:auto;margin-right:auto" >
+
+                       		<tr>
+                       			NAMA = FENDI SETIAWAN
+
+                       		</tr>
+                       		<tr>
+                       		NIM = 191240000910
+                       		</tr>
+                       		<tr>
+                       		PRODI TEKHNIK INFORMATIKA
+                       		</tr>
+                       		<tr>
+                       		SEMESTER 3191240000910</tr>
+
+
+
+                       	</table>
+</body>
+</html>
---
+++ login/koneksi.php
@@ -0,0 +1,3 @@
+<?php
+	$koneksi = mysqli_connect("localhost","id15963910_dblogin2","Driftrace123;", "id15963910_dblogin");
+?>
